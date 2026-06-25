9a1ce61de79008314595d1895d21a9f78750b15d
rehamabdelazime <reham.abdelazime@gmail.com>
2026-06-25 23:20:38 +0300

    test-fixture: add basic react example

---
+++ examples/react-basic/src/App.tsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <>
+      <h1>Welcome</h1>
+
+      <button>Start Game</button>
+
+      <input placeholder="Search..." />
+
+      <div className="hero" />
+    </>
+  );
+}
